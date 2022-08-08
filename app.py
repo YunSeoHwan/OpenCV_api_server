3d6d7952babc5aea265ec3ee24ddebe3da34e1bb
SeoHwan Yun <48356954+YunSeoHwan@users.noreply.github.com>
2022-08-08 13:43:30 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,31 +4,10 @@ import cv2
 from flask import Flask, request
 import requests
 import numpy as np
-from flask_restx import Api, Resource
-from todo import Todo
 
 # Flask 객체 인스턴스
 app = Flask(__name__)
 
-# swagger 경로설정
-api = Api(
-    app,
-    version='0.1',
-    title="OpenCV API Server",
-    description="JustKode's Todo API Server!",
-    terms_url="/",
-    license="MIT"
-)
-
-
-api.add_namespace(Todo, '/todos')
-
-# if __name__ == "__main__":
-#     app.run(debug=True, host='0.0.0.0', port=80)
-@api.route('/hello')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
-class HelloWorld(Resource):
-    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
-        return {"hello": "world!"}
 # Image변환 Class
 class OpenCV():
 
@@ -91,9 +70,15 @@ def image_position():
     # center 반환 메소드
     center = oc.detectImage(json['baseImgPath'], json['detectImgPath'])
 
+    # status
+    if center[0] == -1 and center[1] == -1:
+        # 입력값 없을 시 201 표시
+        stats = 201
+    else:
+        stats = 200
     # 반환 형식
     output={
-        "status": 200,
+        "status": stats,
         "data" : {
             'x':center[0],
             'y':center[1]
@@ -104,4 +89,4 @@ def image_position():
 
 if __name__ == '__main__':
     # 코드 수정 시 반영
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True)
--- todo.py
+++ todo.py
@@ -1,49 +1,42 @@
 from flask import request
-from flask_restx import Resource, Api, Namespace
+from flask_restx import Resource, Namespace, fields
 
 
 todos = {}
 count = 1
 
-
+# namespace 생성
 Todo = Namespace(
      name="OpenCV",
-    description="OpenCV image 좌표처리 API.",
+    description="OpenCV image 좌표처리 API",
 )
 
+# 입력값 model
+todo_fields = Todo.model('detectImage', {  
+    'screenshotPath': fields.String(description='URL or ImagePath', required=True, example="https://screenshot.com/11.png"),
+    'detectImagePath': fields.String(description='URL or ImagePath', required=True, example="https://detectImage/11.png")
+})
+
+# 성공 model
+todo_fields_success = Todo.model('success_output', {  
+    "status": fields.Integer(required=True, example="200"),
+    "x": fields.Integer(required=True, example="157.0"),
+    "y": fields.Integer(required=True, example="12.0")
+        
+})
+
+# value_none model
+todo_fields_value_none = Todo.model('value_none_output',{
+    "status": fields.Integer(required=True, example="201"),
+    "x": fields.Integer(required=True, example="-1"),
+    "y": fields.Integer(required=True, example="-1")
+})
+
+# swagger domain
 @Todo.route('')
 class TodoPost(Resource):
+    @Todo.expect(todo_fields)
+    @Todo.response(200, 'Success', todo_fields_success)
+    @Todo.response(201, 'Value None', todo_fields_value_none)
     def post(self):
-        global count
-        global todos
-        
-        idx = count
-        count += 1
-        todos[idx] = request.json.get('data')
-        
-        return {
-            'todo_id': idx,
-            'data': todos[idx]
-        }
-
-
-@Todo.route('/<int:todo_id>')
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {
-            'todo_id': todo_id,
-            'data': todos[todo_id]
-        }
-
-    def put(self, todo_id):
-        todos[todo_id] = request.json.get('data')
-        return {
-            'todo_id': todo_id,
-            'data': todos[todo_id]
-        }
-    
-    def delete(self, todo_id):
-        del todos[todo_id]
-        return {
-            "delete" : "success"
-        }
+        """screenshotPath와 detectImagePath입력받아 Center 좌표를 반환합니다."""
