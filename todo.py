1fc76929fb7701b7af45ce3976371a0947bfc58c
SeoHwan Yun <48356954+YunSeoHwan@users.noreply.github.com>
2022-08-22 13:55:15 +0900

    Add files via upload

--- todo.py
+++ todo.py
@@ -1,3 +1,4 @@
+from doctest import Example
 from flask import request
 from flask_restx import Resource, Namespace, fields
 
@@ -12,14 +13,14 @@ Todo = Namespace(
 )
 
 # 입력값 model
-todo_fields = Todo.model('detectImgPath', {  
+todo_fields = Todo.model('ImgPath', {  
     'baseImgPath': fields.String(description='URL or ImagePath', required=True, example="https://screenshot.com/11.png"),
     'detectImgPath': fields.String(description='URL or ImagePath', required=True, example="https://detectImage/11.png")
 })
 
 # 성공 model
 todo_fields_success = Todo.model('success_output', {  
-    "status": fields.Integer(required=True, example="200"),
+    "status": fields.Integer(required=True, example="201"),
     "x": fields.Integer(required=True, example="157.0"),
     "y": fields.Integer(required=True, example="12.0")
         
@@ -27,16 +28,24 @@ todo_fields_success = Todo.model('success_output', {
 
 # value_none model
 todo_fields_value_none = Todo.model('value_none_output',{
-    "status": fields.Integer(required=True, example="201"),
+    "status": fields.Integer(required=True, example="204"),
     "x": fields.Integer(required=True, example="-1"),
     "y": fields.Integer(required=True, example="-1")
+})
+
+# url error model
+todo_fields_url_error = Todo.model('url_error_output',{
+    "status": fields.Integer(description="404",required=True, example="404"),
+    "contents": fields.String(description="Error name", required=True, example="detectImagePath is Error")
 })
 
 # swagger domain
 @Todo.route('')
 class TodoPost(Resource):
     @Todo.expect(todo_fields)
-    @Todo.response(200, 'Success', todo_fields_success)
-    @Todo.response(201, 'Value None', todo_fields_value_none)
+    @Todo.response(201, 'Success', todo_fields_success)
+    @Todo.response(204, 'Value None', todo_fields_value_none)
+    @Todo.response(404, 'URL Error', todo_fields_url_error)
+
     def post(self):
         """baseImgPath detectImgPath Center 좌표를 반환합니다."""
