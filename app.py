8b8f2102c3dc6f52f44a8bba6f11f357c72a78b6
SeoHwan Yun <48356954+YunSeoHwan@users.noreply.github.com>
2022-08-23 14:31:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from flask_restx import Api
 from todo import Todo
 
 # Flask 객체 인스턴스
-
 app = Flask(__name__)
 
 api = Api(
@@ -40,22 +39,28 @@ class OpenCV():
     def detectImage(self, screenshotPath,detectImagePath):
         # URL 정규식 표현
         url = re.compile('^(https?://)[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/[a-zA-Z0-9-_/.?=]*')
+
+        # URL, 내장img 구별 정규식
+        check_url = re.compile('^(https?://)')
+           
         # URL 주소 에러가 발생시 예외처리
         try:
-            if (url.match(screenshotPath) == None) == True or (url.match(detectImagePath) == None) == True:
-                # 각각의 경우 대한 처리
-                if url.match(screenshotPath) == None and url.match(detectImagePath) == None:
-                    return "3"
-                elif url.match(screenshotPath) == None:
-                    return "1"
-                elif url.match(detectImagePath) == None:
-                    return "2"
-            elif url.match(screenshotPath)!= None and url.match(detectImagePath)!= None:
-                sourceimage = self.getUrlImage(screenshotPath)
-                template = self.getUrlImage(detectImagePath)
-                
-            # python url 정규식 사용
-            # url error test해보기
+            if check_url.match(screenshotPath) != None or check_url.match(detectImagePath) != None:
+                if (url.match(screenshotPath) == None) == True or (url.match(detectImagePath) == None) == True:
+
+                    # 각각의 경우 대한 처리
+                    if url.match(screenshotPath) == None and url.match(detectImagePath) == None:
+                        return "3"
+                    elif url.match(screenshotPath) == None:
+                        return "1"
+                    elif url.match(detectImagePath) == None:
+                        return "2"
+
+                # 정상 URL의 경우
+                elif url.match(screenshotPath)!= None and url.match(detectImagePath)!= None:
+                    sourceimage = self.getUrlImage(screenshotPath)
+                    template = self.getUrlImage(detectImagePath)
+            
             # 일반적인 경우(.png, .jpg ...)
             else:
                 # imread : img 경로를 읽어와 3차원 행렬로 return 
@@ -63,38 +68,40 @@ class OpenCV():
                 template = cv2.imread(detectImagePath, 0)
                 # Image값이 존재하지 않을 때 (-1, -1) 반환
                 if type(sourceimage) == NoneType or type(template) == NoneType:
-                    return (-1,-1)
-        except Exception as e:
-            return e
+                    return "6"
         
-        # 찾을 이미지의 가로, 세로 너비 할당
-        # 흑백 이미지의 경우 height, width를 받아옴. color는 channel(BGR)값까지
-        # imread를 사용하면 거꾸로 값을 받아옴, 따라서 뒤에서부터 값 할당
-        w, h=template.shape[::-1]
+            # 찾을 이미지의 가로, 세로 너비 할당
+            # 흑백 이미지의 경우 height, width를 받아옴. color는 channel(BGR)값까지
+            # imread를 사용하면 거꾸로 값을 받아옴, 따라서 뒤에서부터 값 할당
+            w, h=template.shape[::-1]
 
-        # 흑백처리 메소드
-        # 문자열로 받은 매개변수를 실행하는 함수, 가장 성능이 뛰어남(연산이 복잡).
-        method = eval('cv2.TM_CCOEFF_NORMED')
-        
-        # 찾는이미지의 좌표값, 가중치 반환 메소드
-        # 원본 이미지와 찾는 이미지의 매칭 위치탐색
-        # 행렬로 res에 값 저장
-        res = cv2.matchTemplate(sourceimage, template, method) 
+            # 흑백처리 메소드
+            # 문자열로 받은 매개변수를 실행하는 함수, 가장 성능이 뛰어남(연산이 복잡).
+            method = eval('cv2.TM_CCOEFF_NORMED')
+            
+            # 찾는이미지의 좌표값, 가중치 반환 메소드
+            # 원본 이미지와 찾는 이미지의 매칭 위치탐색
+            # 행렬로 res에 값 저장
+            res = cv2.matchTemplate(sourceimage, template, method) 
 
-        # 가장 비슷하지 않은 가중치, 비슷한 가중치, 가중치 낮은 왼쪽 위 모서리, 가중치 높은 왼쪽 위 모서리
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            # 가장 비슷하지 않은 가중치, 비슷한 가중치, 가중치 낮은 왼쪽 위 모서리, 가중치 높은 왼쪽 위 모서리
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
-        # 최댓값을 좌측 상단 좌표로 설정(x, y)
-        top_left = max_loc
-        
-        # 찾은 이미지의 center값
-        center = ((top_left[0] + int(w/2))/2, (top_left[1] + int(h/2))/2)
+            # 최댓값을 좌측 상단 좌표로 설정(x, y)
+            top_left = max_loc
+            
+            # 찾은 이미지의 center값
+            center = ((top_left[0] + int(w/2))/2, (top_left[1] + int(h/2))/2)
 
-        return center
+            return center
+            
+        # 예외처리
+        except requests.exceptions.ConnectionError:
+            return "4"
 
-# @app.errorhandler(500)
-# def handle(_error):
-#     return make_response(jsonify({'error' : 'Not found'}), 500)
+        except cv2.error:
+            return "5"
+
 # POST Server
 @app.route('/image-position', methods=['POST'])
 def image_position():
@@ -110,12 +117,7 @@ def image_position():
     center = oc.detectImage(json['baseImgPath'], json['detectImgPath'])
 
     # status
-    if center[0] == -1 and center[1] == -1:
-        # 입력값 없을 시 204 표시
-        msg = "Check your image"
-        error = "Image is None."
-
-    elif center == "1":
+    if center == "1":
         msg = "Check your baseImgPath"
         error = "baseImgPath is Error"
 
@@ -127,9 +129,16 @@ def image_position():
         msg = "Check your All image"
         error = "All path Error"
                 
-    elif type(center) is not str and type(center) is not tuple:
-        return make_response(jsonify({'error' : center}), 500)
-        
+    elif center == "4":
+        return make_response(jsonify({'ConnectionError' : 'Check your url'}), 404)
+
+    elif center == "5":
+        return make_response(jsonify({'cv2.error' : 'Check your url'}), 500)
+
+    elif center == "6":
+        msg = "Check your image"
+        error = "Image is None."
+
     else:
         stats = "Success"
     # 반환 형식
@@ -150,12 +159,11 @@ def image_position():
         output={
             "message": msg,
             "error": error,
-            
         }
         return make_response(jsonify(output), 400)       
 
-    # return(output)
 api.add_namespace(Todo, '/image-position')
+
 if __name__ == '__main__':
     # 코드 수정 시 반영
     app.run(debug=True)
